eb5f7d61dcc5d232a1bbe1d1db7b6a461ffee036
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2013-10-29 09:15:17 -0500

    Add two merge sorters.

---
+++ src/edu/grinnell/csc207/sorting/IterativeMergeSorter.java
@@ -0,0 +1,29 @@
+package edu.grinnell.csc207.sorting;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * Sort using iterative merge sort.
+ *
+ * @author Samuel A. Rebelsky
+ * @author Your Name Here.
+ */
+public class IterativeMergeSorter<T> extends SorterBridge<T> {
+   /**
+    * Sort vals using iterative merge sort.  See the Sorter<T> interface
+    * for additional details.
+    */
+   @Override
+   public T[] sorti(T[] vals, Comparator<T> order) {
+       // STUB
+       int size = 1;
+       while (size < vals.length) {
+           // Merge neighboring subarrays of size size
+           // FILL IN!
+           // The merged subarrays are now twice as large
+           size *= 2;
+       } // while
+       return vals;
+   } // sorti(T[], Comparator<T>)
+} // IterativeMergeSorter<T>
