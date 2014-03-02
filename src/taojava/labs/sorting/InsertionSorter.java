4def9e45716eb6bcbabd5de7ecee6eb0e68c6d20
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2014-03-02 15:55:18 -0600

    More cleanup of the comments in InsertionSorter.java

--- src/taojava/labs/sorting/InsertionSorter.java
+++ src/taojava/labs/sorting/InsertionSorter.java
@@ -46,9 +46,9 @@ public class InsertionSorter<T>
   void insert(T[] vals, Comparator<T> order, int n)
   {
     // Invariants:
-    //    I1(i): Utils.sorted(values,0,i).
-    //    I2(i): Utils.sorted(values,i+1,n).
-    //    I3(i): For all l and r, 0 <= l <= i, i < r <= n,
+    //   I1(i): Utils.sorted(values,0,i).
+    //   I2(i): Utils.sorted(values,i+1,n).
+    //   I3(i): For all l and r, 0 <= l <= i, i < r <= n,
     //           order.compare(vals[l],vals[r]) <= 0
     // Analysis:
     //   I1(n) holds at the because it's a precondition.
@@ -58,22 +58,23 @@ public class InsertionSorter<T>
     while ((i > 0) && (order.compare(vals[i - 1], vals[i]) > 0))
       {
         Utils.swap(vals, i, i - 1);
-        // I1(i-1) holds, but I1(i) does not hold, because we put an
-        // an "unknown" element at position i-1.
-        // I2(i-1) holds b/c all the values in position 0..(i-1) were
-        // less than the values in positions (i+1)..n by I3
-        // I3(i-1) hold b/c we know that the value at position i-1 was
-        // the largest.
-        // We restore the invariant by subtracting 1 from i.
+        // Analysis:
+        //   I1(i-1) holds, but I1(i) does not hold, because we put an
+        //    an "unknown" element at position i-1.
+        //   I2(i-1) holds b/c all the values in position 0..(i-1) were
+        //    less than the values in positions (i+1)..n by I3
+        //   I3(i-1) holds b/c we know that the value at position i-1 was
+        //    the largest.
+        // Conclusion:
+        //   We can restore the invariant by subtracting 1 from i.
         i--;
       } // while
 
     // At this point, either i is 0, in which case I2 tells us that
     // that the elements in position 1..n are sorted and I3 tells us
     // that the element in position 0 is smaller than all of those, or
-    // is is positive, in which case we know that the left part is
+    // i is positive, in which case we know that the left part is
     // sorted (I1), the right part is sorted (I2), and the element at
     // the boundary is in the right position.
-    // condition
   } // insert(T[], Comparator<T>, int)
 } // InsertionSorter<T>
